9addf552a2d44161c27d5e53f5444ff1929af229
dylancallaway <dylancallaway@berkeley.edu>
2019-11-26 14:18:29 -0800

    added path

--- fw/main.cpp
+++ fw/main.cpp
@@ -15,8 +15,8 @@ void heartbeat_fcn();
 // white is +3.3/5V
 // black is gnd
 static int8_t enc_lookup_table[16] = {0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0};
-InterruptIn enc[2][2] = {{PB_14, PB_13},  // encoder 0
-                         {PA_11, PB_12}}; // encoder 1
+InterruptIn enc[2][2] = {{PB_13, PB_14},  // encoder 0
+                         {PB_12, PA_11}}; // encoder 1
 void enc_0_isr();
 void enc_1_isr();
 f_t enc_isr_ptr[2] = {&enc_0_isr, &enc_1_isr};
@@ -42,6 +42,13 @@ int64_t motor_set[2] = {0, 0};
 // global flags
 bool moving[2] = {false, false};
 
+// action pins
+DigitalOut water = PH_1;
+DigitalOut plant = PH_0;
+DigitalOut plant_dir = PC_15;
+DigitalOut probe = PC_14;
+DigitalOut probe_dir = PA_14;
+
 int main()
 {
     for (uint8_t m = 0; m < 2; m++) // iterate through motors
@@ -57,33 +64,107 @@ int main()
             enc[m][p].fall(enc_isr_ptr[m]);
         }
     }
-    heartbeat_thread.start(heartbeat_fcn); // start heartbeat after succesful inits
-
     // init with status LEDs off
     grn_led.write(0);
     red_led.write(0);
 
-    // action flags
+    heartbeat_thread.start(heartbeat_fcn); // start heartbeat after succesful inits
 
-    // action arrays
-    uint8_t num_actions = 2;
-    int32_t action_delay[num_actions] = {3000, 1000};
-    int64_t motor_position[2][num_actions] = {{1000, 3000},  // motor 0
-                                              {2000, 3000}}; // motor 1
+// action flags
+
+// action arrays
+#define STEPS_15DEG 6130 // update for new chassis
+#define STEPS_1IN 1128
+#define MOVE_PROBE 4 * STEPS_1IN
+#define MOVE_WATER 2 * STEPS_1IN
+    const uint8_t num_actions = 61;
+    uint16_t action_delay[num_actions] = {3000, 1000, 1000};
+    int64_t motor_delta[2][num_actions] = {
+        // motor 0
+        {0, MOVE_PROBE, 0, MOVE_WATER, 0, 24 * STEPS_1IN, 0, MOVE_PROBE, 0, MOVE_WATER, 0, 24 * STEPS_1IN, 0, MOVE_PROBE, 0, MOVE_WATER, 0,
+         12 * STEPS_1IN, 6 * STEPS_15DEG, 12 * STEPS_1IN, 6 * STEPS_15DEG, 12 * STEPS_1IN,
+         0, MOVE_PROBE, 0, MOVE_WATER, 0, 24 * STEPS_1IN, 0, MOVE_PROBE, 0, MOVE_WATER, 0, 24 * STEPS_1IN, 0, MOVE_PROBE, 0, MOVE_WATER, 0,
+         12 * STEPS_1IN, 0, 12 * STEPS_1IN, 0, 12 * STEPS_1IN,
+         0, MOVE_PROBE, 0, MOVE_WATER, 0, 24 * STEPS_1IN, 0, MOVE_PROBE, 0, MOVE_WATER, 0, 24 * STEPS_1IN, 0, MOVE_PROBE, 0, MOVE_WATER, 0},
+
+        // motor 1
+        {0, MOVE_PROBE, 0, MOVE_WATER, 0, 24 * STEPS_1IN, 0, MOVE_PROBE, 0, MOVE_WATER, 0, 24 * STEPS_1IN, 0, MOVE_PROBE, 0, MOVE_WATER, 0,
+         12 * STEPS_1IN, 0, 12 * STEPS_1IN, 0, 12 * STEPS_1IN,
+         0, MOVE_PROBE, 0, MOVE_WATER, 0, 24 * STEPS_1IN, 0, MOVE_PROBE, 0, MOVE_WATER, 0, 24 * STEPS_1IN, 0, MOVE_PROBE, 0, MOVE_WATER, 0,
+         12 * STEPS_1IN, 6 * STEPS_15DEG, 12 * STEPS_1IN, 6 * STEPS_15DEG, 12 * STEPS_1IN,
+         0, MOVE_PROBE, 0, MOVE_WATER, 0, 24 * STEPS_1IN, 0, MOVE_PROBE, 0, MOVE_WATER, 0, 24 * STEPS_1IN, 0, MOVE_PROBE, 0, MOVE_WATER, 0}};
+
+    bool planting[num_actions] = {0, 0, 0};
+    bool probing[num_actions] = {0, 0, 0};
+    bool watering[num_actions] = {0, 1, 0};
 
     //action loop
     for (int i = 0; i < num_actions; i++)
     {
+        ThisThread::sleep_for(action_delay[i]);
+
+        // plant
+        if (planting[i] == true)
+        {
+            plant_dir.write(1);
+            for (int pss = 0; pss > 300; pss++)
+            {
+                plant.write(1);
+                ThisThread::sleep_for(5);
+                plant.write(0);
+                ThisThread::sleep_for(5);
+            }
+            ThisThread::sleep_for(500);
+            plant_dir.write(0);
+            for (int pss = 0; pss > 300; pss++)
+            {
+                plant.write(1);
+                ThisThread::sleep_for(5);
+                plant.write(0);
+                ThisThread::sleep_for(5);
+            }
+        }
+
+        // probe
+        if (probing[i] == true)
+        {
+            probe_dir.write(1);
+            for (int pss = 0; pss > 300; pss++)
+            {
+                probe.write(1);
+                ThisThread::sleep_for(5);
+                probe.write(0);
+                ThisThread::sleep_for(5);
+            }
+            ThisThread::sleep_for(500);
+            probe_dir.write(0);
+            for (int pss = 0; pss > 300; pss++)
+            {
+                probe.write(1);
+                ThisThread::sleep_for(5);
+                probe.write(0);
+                ThisThread::sleep_for(5);
+            }
+        }
+
+        // water
+        if (watering[i] == true)
+        {
+            water.write(1);
+            ThisThread::sleep_for(3000);
+            water.write(0);
+        }
+
+        // move
         for (uint8_t m = 0; m < 2; m++)
         {
-            motor_set[m] = motor_position[m][i];
+            motor_set[m] = enc_count[m] + motor_delta[m][i];
             moving[m] = true;
         }
         while (moving[0] == true || moving[1] == true)
         {
-            ThisThread::sleep_for(100);
+            ThisThread::sleep_for(250);
         }
-        ThisThread::sleep_for(action_delay[i]);
     }
 
     // Infinite loop
@@ -125,7 +206,7 @@ void controller_0_fcn()
     out += Kd * (err - last_err); // Derivative
     last_err = err;               // Update last_error to be current err for next loop
 
-    mot_dir[0].write(out / abs(out) == -1); // OPPOSITE TO M2
+    mot_dir[0].write(out / abs(out) == 1); // OPPOSITE TO M1
 
     if (err >= -1 && err <= 1)
     {
@@ -164,7 +245,7 @@ void controller_1_fcn()
     out += Kd * (err - last_err); // Derivative
     last_err = err;               // Update last_error to be current err for next loop
 
-    mot_dir[1].write(out / abs(out) == 1); // OPPOSITE TO M1
+    mot_dir[1].write(out / abs(out) == -1); // OPPOSITE TO M0
 
     if (err >= -1 && err <= 1)
     {
